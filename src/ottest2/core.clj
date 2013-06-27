195485a9f770166b37cdeabd5752f7674674111e
Mat Schaffer <matschaffer@netflix.com>
2013-06-27 12:08:03 -0700

    Kin-n-kaboodle

---
+++ src/ottest2/core.clj
@@ -0,0 +1,7 @@
+(ns ottest2.core
+  (:require [overtone.live :as ot]))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/ottest2/core_test.clj
@@ -0,0 +1,7 @@
+(ns ottest2.core-test
+  (:use clojure.test
+        ottest2.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
